19c621b2350849322c5a6b1db58579fcd49c4141
Harsha <harsha.cognitive@phonepe.com>
2020-08-15 00:16:18 +0530

    Initial Commit

---
+++ WiproAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproAssignment
+//
+//  Created by Harsha Gunaki on 15/08/20.
+//  Copyright © 2020 Harsha Gunaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WiproAssignmentTests/WiproAssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  WiproAssignmentTests.swift
+//  WiproAssignmentTests
+//
+//  Created by Harsha Gunaki on 15/08/20.
+//  Copyright © 2020 Harsha Gunaki. All rights reserved.
+//
+
+import XCTest
+@testable import WiproAssignment
+
+class WiproAssignmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
